689e7ad6b53bb7284298a65ab3e6dd3d10ac4c06
suyeonchoz <167528805+suyeonchoz@users.noreply.github.com>
2024-04-28 09:53:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const keep_alive = require('./keep_alive.js')
 
 const client = new Discord.Client();
 
-let statuses = ['.gg/chillzone', '.gg/socialized']; // Add more statuses here
+let statuses = ['.gg/chillzone', 'You are my sunshine' , 'My only sunshine' , 'You make me happy' , 'When skies are gray']; // Add more statuses here
 let currentStatusIndex = 0;
 
 client.on('ready', () => {
